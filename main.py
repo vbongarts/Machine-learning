779f640eef22466dc5bfc84b5081a828924027e6
Vincent Bongartz <110112@st-maartenscollege.nl>
2020-12-17 10:03:13 +0000

    ik heb een class gemaakt van MyAgent

---
+++ main.py
@@ -0,0 +1,12 @@
+import random 
+
+
+class MyAgent(MLAgent):
+    def evaluate(self,board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
