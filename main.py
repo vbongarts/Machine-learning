0e59c6436b4464668112a6136a0feaa81ecc4f20
Vincent Bongartz <110112@st-maartenscollege.nl>
2020-12-17 14:44:19 +0000

    from weggehaald

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random 
 
-from bke import MLAgent, is_winner, opponent, RandomAgent, train_and_plot, load, save, validate, plot_validation
+import MLAgent, is_winner, opponent, RandomAgent, train_and_plot, load, save, validate, plot_validation
 
 class MyAgent(MLAgent):
     def evaluate(self,board):
