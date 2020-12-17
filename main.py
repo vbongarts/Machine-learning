7f8b463876059bfdafb59bd09764d0e4357683a7
Vincent Bongartz <110112@st-maartenscollege.nl>
2020-12-17 14:40:06 +0000

    geprobeerd af te maken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random 
 
+from bke import MLAgent, is_winner, opponent, RandomAgent, train_and_plot, load, save, validate, plot_validation
 
 class MyAgent(MLAgent):
     def evaluate(self,board):
@@ -12,9 +13,22 @@ class MyAgent(MLAgent):
         return reward
 random.seed(1)
 
+my_agent = MyAgent(alpha=(0.03), epsilon=(0.5))
+
+
+random_agent = RandomAgent()
 train_and_plot(
     agent=my_agent,
     validation_agent=random_agent,
     iterations=90,
     trainings=100,
     validations=1000)
+
+my_agent.learning = False
+save(my_agent, "MyAgentHoogsteRating")
+
+
+validation_agent = RandomAgent()
+
+validation_result= validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
+plot_validation(validation_result)
